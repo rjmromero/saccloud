dd89c60d088cbf499d0f14e51b5f4c94edcee1df
Abel Suárez <lebazeraus@gmail.com>
2022-06-30 09:17:23 -0500

    deleted

--- .nuxt-0.5.1/dist/api/ipx.js
+++
@@ -1,3 +0,0 @@
-import { createIPX, createIPXMiddleware } from "ipx";
-const ipx = createIPX({"dir":"/home/runner/work/saccloud/saccloud/static","domains":[],"sharp":{},"alias":{}});
-export default createIPXMiddleware(ipx);
